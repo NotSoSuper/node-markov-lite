89d13dd7e705ee1d3624f0cd41da2b0a6f52221e
NotSoSuper <xnotsosuper@gmail.com>
2017-08-17 10:15:08 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-markov-lite',
+      author='NotSoSuper',
+      url='https://github.com/NotSoSuper/python-markov-lite',
+      version='1.0',
+      packages=['markov'],
+      license='MIT',
+      description='A python port of node-markov-lite',
+      include_package_data=True
+)
